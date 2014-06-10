01731fa6c073f7b994fd03c335e0439f26ad4009
Nandor Kracser <bonifaido@gmail.com>
2014-06-10 20:16:55 +0200

    added reading of raw TSC values

--- osxcpuinfo/osxcpuinfo.c
+++ osxcpuinfo/osxcpuinfo.c
@@ -10,7 +10,7 @@
 #include <mach/mach_time.h>
 #include <mach/mach_types.h>
 
-kern_return_t osxcpuinfo_start(kmod_info_t * ki, void *d);
+kern_return_t osxcpuinfo_start(kmod_info_t *ki, void *d);
 kern_return_t osxcpuinfo_stop(kmod_info_t *ki, void *d);
 
 // from kern unsupported
@@ -31,6 +31,13 @@ static void cpuid(int op1, int op2, int *data)
         : "a"(op1), "c"(op2));
 }
 
+static uint64_t rdtsc()
+{
+    unsigned int hi, lo;
+    asm("rdtsc" : "=a" (lo), "=d" (hi));
+    return ((uint64_t)hi << 32) | lo;
+}
+
 typedef struct
 {
     int os_id;
@@ -39,32 +46,38 @@ typedef struct
     int apic;
 } cpu_info_t;
 
+typedef struct
+{
+    uint64_t raw;
+    uint64_t corrected;
+} tsc_t;
+
 static void cpu_info_callback(void *data)
 {
     int values[4];
     cpu_info_t *cpus = (cpu_info_t *) data;
     int cpu = cpu_number();
-    
+
     cpus[cpu].os_id = cpu;
-    
+
     cpuid(0xB, 0, values);
     cpus[cpu].core = values[3] >> values[0] & 0xF;
-    
+
     cpuid(0xB, 1, values);
     cpus[cpu].socket = values[3] >> values[0] & 0xF;
-    
+
     cpus[cpu].apic = values[3];
 }
 
 static void get_cpu_info_data()
 {
     cpu_info_t values[NCPUS];
-    
+
     mp_rendezvous(NULL, (void (*)(void *))cpu_info_callback,
                   NULL, (void *) values);
-    
+
     printf("---- get_cpu_info_data ----\n");
-    
+
     for (int i = 0; i < NCPUS; i++)
     {
         printf("-- cpu_info_t: os_id %d socket %d, core %d, apic %d\n",
@@ -74,8 +87,10 @@ static void get_cpu_info_data()
 
 static void get_cpu_tick_diff_callback(void *data)
 {
-    uint64_t *values = (uint64_t *) data;
-    values[cpu_number()] = mach_absolute_time();
+    tsc_t *values = (tsc_t *) data;
+    int cpu = cpu_number();
+    values[cpu].raw = rdtsc();
+    values[cpu].corrected = mach_absolute_time();
 }
 
 static void get_cpu_tick_diff()
@@ -83,23 +98,23 @@ static void get_cpu_tick_diff()
     // to get nanos:
     // static mach_timebase_info_data_t timebase_info;
     // mach_timebase_info(&timebase_info);
-    uint64_t values[NCPUS];
-    
+    tsc_t values[NCPUS];
+
     mp_rendezvous(NULL, get_cpu_tick_diff_callback, NULL, (void *) values);
-    
+
     printf("---- get_cpu_tick_diff ----\n");
-    
+
     for (int i = 0; i < NCPUS; i++)
     {
-        printf("-- cpu: %d time: %llu --\n", i, values[i]);
+        printf("-- cpu: %d raw: %llu corrected: %llu --\n", i, values[i].raw, values[i].corrected);
     }
 }
 
-kern_return_t osxcpuinfo_start(kmod_info_t * ki, void *d)
+kern_return_t osxcpuinfo_start(kmod_info_t *ki, void *d)
 {
     get_cpu_info_data();
     get_cpu_tick_diff();
-    
+
     return KERN_SUCCESS;
 }
 
